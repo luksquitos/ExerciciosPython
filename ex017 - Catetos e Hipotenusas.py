01c448deebce72d4d7677fc1b1efe807accbe7b5
Lucas Martins <lucasmartinscaet@hotmail.com>
2021-09-22 20:02:37 -0300

    commit

---
+++ ex017 - Catetos e Hipotenusas.py
@@ -0,0 +1,14 @@
+#Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo.
+# Calcule e mostre o comprimento da hiponenusa.
+
+import math
+
+cat1 = float(input("Digite o valor do Cateto Oposto: "))
+cat2 = float(input("Digite o valor do Cateto Adjacente: "))
+
+pow(cat1, 2, cat2, 2)
+
+
+
+
+
