5ad7c5a6861bd989293000ae4409cc989fe4cc79
Lucas Martins <lucasmartinscaet@hotmail.com>
2021-10-31 14:33:25 -0300

    bases numericas

--- ex037 - Conversor de Bases Numéricas.py
+++ ex037 - Conversor de Bases Numéricas.py
@@ -1,11 +1,19 @@
-#Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão:
+# Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão:
 # 1 para binário, 2 para octal e 3 para hexadecimal.
 
 print('Digite um número para a conversão: ')
 num = int(input('- '))
-x = num / 2
-y = num % 2
+print('''Você gostaria de converter para qual dos a seguir ?
+[A]- Binário
+[B]- Hexadecimal
+[C]- Octal''')
+opcao = input('Digite uma opcão: ').upper()[0]
 
-print(x, '\n', y)
-print(x / 2 != 0)
-
+while not opcao in 'ABC:':
+    opcao = input('Opcão inválida, tente novamente: ').upper()
+if opcao == 'A':
+    print(f'O número {num} em BINÁRIO é: {bin(num)}')
+elif opcao == 'B':
+    print(f'O número {num} em HEXADECIMAL é: {hex(num)}')
+elif opcao == 'C':
+    print(f'O número {num} em OCTAL é : {oct(num)}')
