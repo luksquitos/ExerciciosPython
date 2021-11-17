4c4f24115013c602ebf0cfb1b265b375ab1875fd
Lucas Martins <lucasmartinscaet@hotmail.com>
2021-11-17 15:44:14 -0300

    listas pt2

--- ex088 - Palpites para a Mega Sena.py
+++ ex088 - Palpites para a Mega Sena.py
@@ -3,6 +3,7 @@
 
 from random import randint
 from datetime import datetime
+from time import sleep
 
 print(f'\033[1;32m{f"MEGA SENA {datetime.today().year}":.^50}')
 cont = int(input('Quantos jogos você tentará esse ano ?\n- '))
@@ -11,15 +12,20 @@ jogosGerados = []
 numeros = 6
 
 while cont > 0:
-    gaveta.append(randint(1, 60))
-    numeros -= 1
+    num = randint(1, 60)
+    if num not in gaveta:  # Para que no mesmo jogo, o programa não sorteie um número repetido
+        gaveta.append(num)
+        numeros -= 1
     if numeros == 0:
+        gaveta.sort()
         jogosGerados.append(gaveta[:])
         gaveta.clear()
         cont -= 1
         numeros = 6
 print('\033[1;34m')
 print('Os palpites gerados foram')
-for jogos in jogosGerados:
-    print(jogos)
+for num, jogos in enumerate(jogosGerados):
+    sleep(1)
+    print(f'Jogo {num + 1}: {jogos}')
+sleep(1)
 print('BOA SORTE!!!')
